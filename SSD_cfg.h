d235f00e528499d258d01a9125e353d8492c13cd
Ahmed.Ashraf <ahmed.ashraf@coretech-innovations.com>
2023-02-23 14:51:14 +0200

    merging files

---
+++ SSD_cfg.h
@@ -0,0 +1,13 @@
+/*
+ * SSD_cfg.h
+ *
+ *  Created on: Feb 23, 2023
+ *      Author: Osama.Mahmoud
+ */
+
+#ifndef SSD_CFG_H_
+#define SSD_CFG_H_
+
+#define   Copy_u8SEGNumber    2
+
+#endif /* SSD_CFG_H_ */
