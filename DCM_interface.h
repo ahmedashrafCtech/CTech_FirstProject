6feb8f3fa890881dbeee8aab5c19dd2f286ce622
Abdullah Abomosallam <Abdullah.Abomosallam@coretech-innovations.com>
2023-02-23 12:15:59 +0200

    DC motor files

---
+++ DCM_interface.h
@@ -0,0 +1,35 @@
+#include"../SERVICES/STD_TYPES.h"
+#include"../DIO/DIO_interface.h"
+
+
+#define DCM_FORWARD 1
+#define DCM_REVERSE 0
+
+#define DCM_PORTA  0
+#define DCM_PORTB  1
+#define DCM_PORTC  2
+#define DCM_PORTD  3
+
+
+#define DCM_PIN0   0
+#define DCM_PIN1   1
+#define DCM_PIN2   2
+#define DCM_PIN3   3
+#define DCM_PIN4   4
+#define DCM_PIN5   5
+#define DCM_PIN6   6
+#define DCM_PIN7   7
+
+
+
+typedef struct 
+{
+	u8 port;
+	u8 pin ;
+	u8 connection;
+	
+} DCM_TYPE;
+
+void DCM_voidDCMOn(DCM_TYPE copy_DCMStructDCM1);
+void DCM_voidDCMOff(DCM_TYPE copy_DCMStructDCM1);
+void DCM_voidInit(DCM_TYPE copy_DCMStructDCM1);
---
+++ DCM_program.c
@@ -0,0 +1,40 @@
+
+#include"DCM_interface.h"
+#include"../MCAL/DIO/DIO_interface.h"
+
+void DCM_voidDCMOn(DCM_TYPE copy_DCMStructDCM1)
+
+{
+	if(copy_DCMStructDCM1.connection==DCM_FORWARD)
+DIO_WrtPin(copy_DCMStructDCM1.port,copy_DCMStructDCM1.pin,Pin_High);
+	else if(copy_DCMStructDCM1.connection==DCM_REVERSE)
+DIO_WrtPin(copy_DCMStructDCM1.port,copy_DCMStructDCM1.pin,Pin_Low);	
+	
+	
+}
+void DCM_voidLedOff(DCM_TYPE copy_DCMStructDCM1)
+{
+	
+	
+	if(copy_DCMStructDCM1.connection==DCM_FORWARD)
+DIO_WrtPin(copy_DCMStructDCM1.port,copy_DCMStructDCM1.pin,Pin_Low);
+	else if(copy_DCMStructDCM1.connection==DCM_REVERSE)
+DIO_WrtPin(copy_DCMStructDCM1.port,copy_DCMStructDCM1.pin,Pin_High);	
+	
+	
+	
+	
+}
+void DCM_voidInit(DCM_TYPE copy_DCMStructDCM1)
+{
+	
+	DIO_SetDir(copy_DCMStructDCM1.port,copy_DCMStructDCM1.pin, Pin_Output);
+	
+	
+	
+	
+	
+	
+}
+
+
