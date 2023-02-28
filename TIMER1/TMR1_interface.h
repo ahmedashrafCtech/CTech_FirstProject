09576a3d6549cabbdced364ee66d699f6c4327ea
asmaamohsen00 <Asmaa.Mohsen@coretech-innovations.com>
2023-02-28 13:14:31 +0200

    updated the software module

--- TIMER1/TMR1_interface.h
+++ TIMER1/TMR1_interface.h
@@ -12,6 +12,25 @@
 #define PRESCALER_256        4
 #define PRESCALER_1024       5
 
+/*different interrupt sources*/
+#define NORMAL               0
+#define CTC                  1
+
+/*different channels for pwm signal*/
+#define CHANNEL_A            0
+#define CHANNEL_B            1
+
+/*different values of output frequency of pwm signal represented by value of ICR1 register*/
+/*considering fixed prescaler = 8 -> tick time = 1us*/
+#define FREQ_1000                         1000
+#define FREQ_500                          2000
+#define FREQ_250                          4000
+#define FREQ_200                          5000
+#define FREQ_125                          8000
+#define FREQ_100                          10000
+#define FREQ_50                           20000
+#define FREQ_25                           40000
+#define FREQ_20                           50000
 
 
 
@@ -24,18 +43,33 @@ void TMR1_voidNormalSync(u8 Copy_u8Prescaler);
 //TESTED>>>>>>>
 void TMR1_voidNormalAsync(u8 Copy_u8Prescaler);
 
-/*this function is to perform call back for application ISR*/
-//TESTED>>>>>>>
-void TMR1_voidSetCallBack(void(*PtrToFunc)(void));
-
 /*this function is to perform 1sec sync delay by CTC mode*/
 //TESTED>>>>>>>
 void TMR1_voidComMatchSync();
 
+/*this function is to perform 1sec sync delay by CTC mode*/
+//TESTED>>>>>>>
+void TMR1_voidComMatchAsync();
+
+/*this function is to generate a pwm signal on channel A or B*/
+/*duty cycle in range [0:100]*/
+//TESTED OVER LED INTENSITY
+void TMR1_voidFastPwm(u8 Copy_u8Channel , u16 Copy_u16Frequency , u8 Copy_u8DutyCycle);
+
+/*this function is to perform a delay period synchronously*/
+//TESTED>>>>>>>
+void TMR1_voidDelaySync(u16 Copy_u16TimeIn_ms);
+
+/*this function is to perform a delay period asynchronously*/
+//TESTED>>>>>>>
+void TMR1_voidDelayAsync(u16 Copy_u16TimeIn_ms);
+
+/*this function is to perform call back for application ISR*/
+//TESTED>>>>>>>
+void TMR1_voidSetCallBack(u8 Copy_u8SrcID,void(*PtrToFunc)(void));
+
 /*this function is to stop TIMER1*/
 //TESTED>>>>>>>
 void TMR1_voidStopTimer1(void);
-
-
 
 #endif
--- TIMER1/TMR1_program.c
+++ TIMER1/TMR1_program.c
@@ -8,6 +8,7 @@
 
 
 void(*GlobalPtrOverFlow)(void) = NULL ;
+void(*GlobalPtrCTC)(void)=NULL ;
 
 /**************************************Functions' implementation**************************************/
 void TMR1_voidNormalSync(u8  Copy_u8Prescaler)
@@ -16,7 +17,7 @@ void TMR1_voidNormalSync(u8  Copy_u8Prescaler)
 	WRITE_BIT(TCCR1A , TCCR1A_WGM10 , 0);
 	WRITE_BIT(TCCR1A , TCCR1A_WGM11 , 0);
 	WRITE_BIT(TCCR1B , TCCR1B_WGM12 , 0);
-	WRITE_BIT(TCCR1A , TCCR1B_WGM13 , 0);
+	WRITE_BIT(TCCR1B , TCCR1B_WGM13 , 0);
 
 	/*2)disconnecting OC1A & OC1B*/
 	WRITE_BIT(TCCR1A,TCCR1A_COM1A0,0);
@@ -44,7 +45,7 @@ void TMR1_voidNormalAsync(u8 Copy_u8Prescaler)
 	WRITE_BIT(TCCR1A , TCCR1A_WGM10 , 0);
 	WRITE_BIT(TCCR1A , TCCR1A_WGM11 , 0);
 	WRITE_BIT(TCCR1B , TCCR1B_WGM12 , 0);
-	WRITE_BIT(TCCR1A , TCCR1B_WGM13 , 0);
+	WRITE_BIT(TCCR1B , TCCR1B_WGM13 , 0);
 
 	/*2)disconnecting OC1A & OC1B*/
 	WRITE_BIT(TCCR1A,TCCR1A_COM1A0,0);
@@ -69,7 +70,7 @@ void TMR1_voidComMatchSync()
 	WRITE_BIT(TCCR1A , TCCR1A_WGM10 , 0);
 	WRITE_BIT(TCCR1A , TCCR1A_WGM11 , 0);
 	WRITE_BIT(TCCR1B , TCCR1B_WGM12 , 1);
-	WRITE_BIT(TCCR1A , TCCR1B_WGM13 , 0);
+	WRITE_BIT(TCCR1B , TCCR1B_WGM13 , 0);
 
 	/*2)disconnecting OC1A & OC1B*/
 	WRITE_BIT(TCCR1A,TCCR1A_COM1A0,0);
@@ -94,10 +95,155 @@ void TMR1_voidComMatchSync()
 
 /*******************************************************************************************************/
 
-void TMR1_voidSetCallBack(void(*PtrToFunc)(void))
+void TMR1_voidComMatchAsync()
 {
-	if(PtrToFunc != NULL)
-		GlobalPtrOverFlow = PtrToFunc ;
+	/*1)set mode to compare match mode*/
+	WRITE_BIT(TCCR1A , TCCR1A_WGM10 , 0);
+	WRITE_BIT(TCCR1A , TCCR1A_WGM11 , 0);
+	WRITE_BIT(TCCR1B , TCCR1B_WGM12 , 1);
+	WRITE_BIT(TCCR1B , TCCR1B_WGM13 , 0);
+
+	/*2)disconnecting OC1A & OC1B*/
+	WRITE_BIT(TCCR1A,TCCR1A_COM1A0,0);
+	WRITE_BIT(TCCR1A,TCCR1A_COM1A1,0);
+	WRITE_BIT(TCCR1A,TCCR1A_COM1B1,0);
+	WRITE_BIT(TCCR1A,TCCR1A_COM1B0,0);
+
+	/*3)setting OCR value*/
+	/*FOR 1SEC DELAY*/
+	OCR1A = 31250 ;
+
+	/*4)enabling both global and peripheral interrupt*/
+	WRITE_BIT(TIMSK , TIMSK_OCIE1A , 1);
+	WRITE_BIT(SREG,7,1);
+
+	/*5)setting prescaler and running timer1*/
+	TCCR1B &= PRESCALER_MASK ;        //clearing bits to add new prescaler value
+	TCCR1B |= PRESCALER_1024 ;
+}
+
+/*******************************************************************************************************/
+
+void TMR1_voidSetCallBack(u8 Copy_u8SrcID,void(*PtrToFunc)(void))
+{
+	switch(Copy_u8SrcID)
+	{
+
+		case NORMAL :
+			if(PtrToFunc != NULL)
+				GlobalPtrOverFlow = PtrToFunc ;
+		break ;
+
+		case CTC :
+			if(PtrToFunc != NULL)
+					GlobalPtrCTC = PtrToFunc ;
+			break ;
+
+	}
+}
+
+/*******************************************************************************************************/
+
+void TMR1_voidFastPwm(u8 Copy_u8Channel , u16 Copy_u16Frequency , u8 Copy_u8DutyCycle)
+{
+	/*Setting running mode to be mode no.14 (fast PWM)*/
+	WRITE_BIT(TCCR1A , TCCR1A_WGM10 , 0);
+	WRITE_BIT(TCCR1A , TCCR1A_WGM11 , 1);
+	WRITE_BIT(TCCR1B , TCCR1B_WGM12 , 1);
+	WRITE_BIT(TCCR1B , TCCR1B_WGM13 , 1);
+
+	/*2)Setting non inverted mode*/
+	/*switching over channels*/
+	switch(Copy_u8Channel)
+	{
+		case CHANNEL_A :
+			WRITE_BIT(TCCR1A , TCCR1A_COM1A0 , 0);
+			WRITE_BIT(TCCR1A , TCCR1A_COM1A1 , 1);
+			break ;
+
+		case CHANNEL_B :
+			WRITE_BIT(TCCR1A , TCCR1A_COM1B0 , 0);
+			WRITE_BIT(TCCR1A , TCCR1A_COM1B1 , 1);
+			break ;
+	}
+
+	/*3)Setting ICR1 value that indicates frequency*/
+	ICR1 = Copy_u16Frequency ;
+
+	/*4)Setting value of OCR1x register that controls duty cycle*/
+	/*switching over channels*/
+	switch(Copy_u8Channel)
+	{
+		case CHANNEL_A :
+			OCR1A = ((u32)Copy_u8DutyCycle * ICR1) / 100;  //mapping values
+			break ;
+
+		case CHANNEL_B :
+			OCR1B = ((u32)Copy_u8DutyCycle * ICR1) / 100;  //mapping values
+			break ;
+	}
+
+	/*5)Running timer tp generate pwm signal*/
+	TCCR1B &= PRESCALER_MASK ;        //clearing bits to add new prescaler value
+	TCCR1B |= PRESCALER_8 ;
+}
+
+/*******************************************************************************************************/
+
+void TMR1_voidDelaySync(u16 Copy_u16TimeIn_ms)
+{
+	/*1)set mode to compare match mode*/
+	WRITE_BIT(TCCR1A , TCCR1A_WGM10 , 0);
+	WRITE_BIT(TCCR1A , TCCR1A_WGM11 , 0);
+	WRITE_BIT(TCCR1B , TCCR1B_WGM12 , 1);
+	WRITE_BIT(TCCR1B , TCCR1B_WGM13 , 0);
+
+	/*2)disconnecting OC1A & OC1B*/
+	WRITE_BIT(TCCR1A,TCCR1A_COM1A0,0);
+	WRITE_BIT(TCCR1A,TCCR1A_COM1A1,0);
+	WRITE_BIT(TCCR1A,TCCR1A_COM1B1,0);
+	WRITE_BIT(TCCR1A,TCCR1A_COM1B0,0);
+
+	/*3)Converting target time to a value fits in OCR1A register*/
+	OCR1A = ((u32)Copy_u16TimeIn_ms * 1000) / 125 ;
+
+	/*4)Setting prescaler and running timer*/
+	TCCR1B &= PRESCALER_MASK ;        //clearing bits to add new prescaler value
+	TCCR1B |= PRESCALER_1024 ;
+
+	/*5)Polling over flag until it rises then clearing it*/
+	while((GET_BIT(TIFR, TIFR_OCF1A))==0);
+	WRITE_BIT(TIFR, TIFR_OCF1A,1);
+
+}
+
+/*******************************************************************************************************/
+
+void TMR1_voidDelayAsync(u16 Copy_u16TimeIn_ms)
+{
+	/*1)set mode to compare match mode*/
+	WRITE_BIT(TCCR1A , TCCR1A_WGM10 , 0);
+	WRITE_BIT(TCCR1A , TCCR1A_WGM11 , 0);
+	WRITE_BIT(TCCR1B , TCCR1B_WGM12 , 1);
+	WRITE_BIT(TCCR1B , TCCR1B_WGM13 , 0);
+
+	/*2)disconnecting OC1A & OC1B*/
+	WRITE_BIT(TCCR1A,TCCR1A_COM1A0,0);
+	WRITE_BIT(TCCR1A,TCCR1A_COM1A1,0);
+	WRITE_BIT(TCCR1A,TCCR1A_COM1B1,0);
+	WRITE_BIT(TCCR1A,TCCR1A_COM1B0,0);
+
+	/*3)Converting target time to a value fits in OCR1A register*/
+	OCR1A = ((u32)Copy_u16TimeIn_ms * 1000) / 125 ;
+
+	/*4)enabling both global and peripheral interrupt*/
+	WRITE_BIT(TIMSK , TIMSK_OCIE1A , 1);
+	WRITE_BIT(SREG,7,1);
+
+	/*5)setting prescaler and running timer1*/
+	TCCR1B &= PRESCALER_MASK ;        //clearing bits to add new prescaler value
+	TCCR1B |= PRESCALER_1024 ;
+
 }
 
 /*******************************************************************************************************/
@@ -111,6 +257,14 @@ void TMR1_voidStopTimer1(void)
 
 /*******************************************************************************************************/
 
+/*isr for CTC on channel A*/
+void __vector_7 (void) __attribute__((signal));
+void __vector_7 (void)
+{
+	GlobalPtrCTC();
+}
+
+/*isr for normal over flow*/
 void __vector_9 (void) __attribute__((signal));
 void __vector_9 (void)
 {
